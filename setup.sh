d72836eef3422aa6688f9a02d4050811d5a2bec9
root <root@ubuntu1.u1>
2013-12-10 22:33:11 +0900

    update

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-## default mode for installation
-## hard_mode = hard_kernel_setup.sh  
-## soft_mode = soft_kernel_setup.sh
-## package_mode = package_setup.sh
-ovs_setup_mode=${ovs_setup_mode:=soft_kernel_setup.sh}
-$(find / -wholename $(pwd)/$ovs_setup_mode)
